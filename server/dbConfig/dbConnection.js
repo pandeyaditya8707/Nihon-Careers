bc2e0e78c3d973704368288b62b290eee3fdc430
pandeyaditya8707 <100992430+pandeyaditya8707@users.noreply.github.com>
2024-05-01 20:02:37 +0530

    Update dbConnection.js

--- server/dbConfig/dbConnection.js
+++ server/dbConfig/dbConnection.js
@@ -5,6 +5,7 @@ const dbConnection = async () => {
   try {
     const dbConnection = await mongoose.connect(process.env.MONGODB_URL);
 
+
     console.log("DB Connected Successfully");
   } catch (error) {
     console.log("DB Error: " + error);
